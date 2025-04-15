9a9cc6968dcd6b8eb9afbed204112362d742fae6
Tz-Hu <tz_hu@outlook.com>
2025-04-15 23:45:25 +0800

    Update train.py

--- train.py
+++ train.py
@@ -13,36 +13,46 @@ from environment import ZorkEnvWrapper
 from ppo_agent import PPOAgent
 from ppo_buffer import PPOBuffer
 
+def filter_copyright(text):
+    keywords = ["Copyright", "Microsoft Corporation", "GNU General Public License", "Serial number", "ZORK is a registered trademark"]
+    lines = text.split('\n')
+    filtered = [line for line in lines if not any(kw in line for kw in keywords)]
+    return '\n'.join(filtered).strip()
+
 def train():
     agent = PPOAgent(device)
     critic = agent.critic.to(device)
     actor = agent.actor.to(device)
-    env = ZorkEnvWrapper()
+    zork_env = ZorkEnvWrapper()
     optimizer = Adam(
     filter(lambda p: p.requires_grad, list(actor.parameters()) + list(critic.parameters())),
     lr=PPO_LR
     )
+    return_log_path = os.path.join(LOG_DIR, "return_log.txt")
     buffer = PPOBuffer()
     for ep in range(1, MAX_EPISODES + 1):
         agent.reset()
-        env.reset()
+        obs, info = zork_env.reset()
         buffer.reset()
         log_path = os.path.join(LOG_DIR, f"episode_{ep:03d}.txt")
         with open(log_path, "w", encoding="utf-8") as log_file:
             log_file.write(f"Episode {ep:03d} 开始\n")
             log_file.write("=" * 80 + "\n")
+            print("=" * 80 )
             for time in range(1, 1 + EPISODE_STEPS):
-                valid_actions = env.get_valid_actions()
-                obs = env.obs
+                valid_actions = zork_env.get_valid_actions()
+                buffer.add_valid_action(valid_actions)
+                obs = filter_copyright(zork_env.obs)
+                buffer.add_obs(obs)
                 action, old_logprob = actor.generate_action_logprob(obs, valid_actions)
                 buffer.add_old_logprob(old_logprob)
-                _, step_score, done, info = env.step(action)
-                buffer.add_done(done)
+                _, step_score, done, info = zork_env.step(action)
+                # buffer.add_done(done)
                 if done:
-                    if env.victory():
-                        log_file.write(f"\n[ WIN ] Moves numbers:{info['moves']}\tTotal Score: {env.get_score()}\n")
+                    if zork_env.victory():
+                        log_file.write(f"\n[ WIN ] Moves numbers:{info['moves']}\tTotal Score: {zork_env.get_score()}\n")
                     else:
-                        log_file.write(f"\n[ DIED] Moves numbers:{info['moves']}\tTotal Score: {env.get_score()}\n")
+                        log_file.write(f"\n[ DIED] Moves numbers:{info['moves']}\tTotal Score: {zork_env.get_score()}\n")
                     log_file.write("=" * 90 + "\n")
                     next_value = 0
                     break
@@ -51,7 +61,7 @@ def train():
                 state_value = next_value
                 buffer.add_state_value(state_value)
 # 从这里入手重写一下 environment的奖励， 记得加 feedback
-                step_reward = env.my_step_reward(action)
+                step_reward = zork_env.my_step_reward(action)
                 buffer.add_step_reward(step_reward)
 
                 log_file.write(f"Time {time} | StepScore: {step_score}")
@@ -60,36 +70,33 @@ def train():
                 log_file.write(f"Step Reward: {step_reward}\n")
                 log_file.write(f"Game Step Score: {step_score}\t Game Total Score: {info['score']}\n")
                 log_file.write("=" * 80 + "\n")
-                print("=" * 80 + "\n")
-                print(f"[E {ep}  t= {time}] \n 观察: {obs} Action: {action.ljust(20)} StepReward: {step_reward:.3f}\tReturn: 哈哈看不到\tStepScore: {step_score}TotalScore: {info['score']}\n")
+                print("=" * 80 )
+                print(f"[E {ep}  t= {time}] \n观察: {obs}\n\nAction: {action}\nStepReward: {step_reward:.3f}\tStepScore: {step_score}\tTotalScore: {info['score']}")
 
             buffer.next_value = next_value
-            print(f"next value检查是否出循环被保留: {nexT_value}\n" )
-
             for epoch in range(PPO_EPOCHS):
+                print("hahahah\n")
                 i = 1
                 for batch in buffer.get_batches(BATCH_SIZE):
+                    print("blahbalh\n")
                     loss = agent.compute_loss(batch)
                     print(f"batch {i}:, loss = {loss:.4f}")
                     log_file.write(f"batch {i}:, loss = {loss}")
-                    
                     optimizer.zero_grad()
                     loss.backward()
                     torch.nn.utils.clip_grad_norm_(list(actor.parameters()) + list(critic.parameters()), MAX_GRAD_NORM)
                     optimizer.step()
                     i +=1
 
+            discnt_returns = agent.compute_total_return(buffer.step_rewards)
+            with open(return_log_path, "a", encoding="utf-8") as log_file:
+                log_file.write(f"Episode {ep:03d} , Move {info['moves']}, discnt Return : {discnt_returns}\tscore: {zork_env.get_score()}\n")
+            torch.cuda.empty_cache()
             # 保存检查点
             # checkpoint_path = os.path.join(SAVE_DIR, f"checkpoint_ep{ep}.pt")
             # agent.save_policy(checkpoint_path)
-            torch.cuda.empty_cache()
 
     # 保存最终模型``
     # agent.save_policy(os.path.join(SAVE_DIR, "final_policy.pt"))
     # print()
-        return_log_path = os.path.join(LOG_DIR, "return_log.txt")
-        with open(return_log_path, "w", encoding="utf-8") as log_file:
-# 没write return等信息
-            log_file.write(f"Episode {ep:03d} , Move {info['moves']}, discnt Return : {discnt_return}")
-    
     print("Training complete.")
