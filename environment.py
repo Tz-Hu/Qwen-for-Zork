4a833b3f15d746686fd2b5bc795531c486d81241
Tz-Hu <tz_hu@outlook.com>
2025-04-16 19:34:34 +0800

    Update environment.py

--- environment.py
+++ environment.py
@@ -2,8 +2,8 @@ from collections import deque
 from jericho import FrotzEnv
 from config import ROM_PATH, GAME_RATIO, MAX_ACTION_HISTORY
 from math import log
+from jericho import util
     
-# ============================ Zork 环境类 ============================
 class ZorkEnvWrapper:
     def __init__(self):
         self.env = FrotzEnv(ROM_PATH)
@@ -16,12 +16,11 @@ class ZorkEnvWrapper:
         self.valid_actions = self.env.get_valid_actions()
         self.action_history = deque(maxlen=MAX_ACTION_HISTORY)
         self.feedback = ""
+        self.current_inventory = []
         self.obs, self.info = self.env.reset()
         return self.obs, self.info
     def step(self, action):
         self.action_history.append(action)
-        self.track_visited_room()
-        self.feedback = ""
         self.obs, self.step_score, self.done, self.info = self.env.step(action)
         return self.obs, self.step_score, self.done, self.info
     def get_valid_actions(self, use_object_tree=True, use_ctypes=True, use_parallel=True):
@@ -52,6 +51,16 @@ class ZorkEnvWrapper:
         current_room_hash = self.get_wold_state_hash()
         if current_room_hash not in self.visited_rooms:
             self.visited_rooms.add(current_room_hash)
+            return True
+        return False
+    def track_touched_obj(self):
+        inventory = self.get_inverntory()
+        for obj in inventory:
+            obj_hash = hash(obj)  # 使用物品的哈希值作为唯一标识
+            if obj_hash not in self.obj_touched: 
+                self.obj_touched.add(obj_hash)
+                return True
+        return False
     def get_inverntory(self):
         ''' Returns a list of :class:`jericho.ZObject` in the player's posession. '''
         return self.env.get_inventory()
@@ -105,12 +114,12 @@ class ZorkEnvWrapper:
         ''' Returns `True` if the game is over and the player has lost. '''
         return self.env.game_over()    
 
-# 1.重复惩罚 暂时写完了
     def repetitive_action_penalty(self, action):
         penalty = 0.0
         # 连续重复动作
         if len(self.action_history) >= 5 and all(a == action for a in list(self.action_history)[-5:]):
             penalty -= 0.3
+            self.feedback = f" Don't repeate {action}\n"
 
         # 循环往返检测，如 ABAB 或 ABCABC
         for cycle_len in [2, 3]:
@@ -119,46 +128,33 @@ class ZorkEnvWrapper:
                 first, second = seq[:cycle_len], seq[cycle_len:]
                 if first == second:
                     penalty -= 3 
+                    self.feedback = f" Don't loop {first} {second}\n"
                     break
         return penalty
     
-# 2.新地点奖励, 结合 visited_room 变量 和 get_wold_state_hash 函数 来写
     def new_place_reward(self):
-        current_room_hash = self.get_wold_state_hash()
-        if current_room_hash not in self.visited_rooms:
-            # self.track_visited_room(current_room_hash)
-            # 奖励值与已访问房间数量成对数增长
-            return 0.2
-        return 0
+        reward = 0.05
+        return reward * self.track_visited_room()
     
-
-# 3.Win/Died 奖惩：
-# 上面有 victory 和 game_over 函数 判断输赢
-# 总体逻辑：基于 moves 奖励大小： 
-# win + move少 > win + move多  >> dead + move多 > dead + move少
-# 若 不赢不输 （步数到了） 返回 0
     def win_loss_reward(self):
+        moves = self.get_moves()
         if self.victory():
-            # 胜利奖励，步数越少奖励越高
-            return 10000 / self.get_moves() 
+            return 10000 / moves
         elif self.game_over():
-            # 失败惩罚，步数越多惩罚越轻
-            return -50 + 2 * self.get_moves()
+            return -10000 / moves
         else:
-            # 游戏未结束
             return 0
+        
+    def unrecognized_panalty(self, action):
+        recognized : bool = util.recognized(action)
+        if not recognized:
+            self.feedback = f" Answer only with NUMBERs(action index)!\n"
+            return -1
+        return 0
 
-# 4.玩家拿到物品奖励
     def obj_reward(self):
-        inventory = self.get_inverntory()
-        reward = 0
-        # for obj in inventory:
-        #     obj_hash = hash(obj)  # 使用物品的哈希值作为唯一标识
-        #     if obj_hash not in self.obj_touched:  # 复用 visited_rooms 记录已获得物品
-        #         self.obj_touched.add(obj_hash)
-        #         reward += 0.7
-        return reward
-
+        reward = 0.7
+        return self.track_touched_obj() * reward
 
     # 5.若 agent 没有改变世界，小幅度扣分
     def change_world_reward(self):
@@ -168,10 +164,11 @@ class ZorkEnvWrapper:
     def my_step_reward(self, action):
         my_step_reward = (
                         self.repetitive_action_penalty(action) 
+                        + self.unrecognized_panalty(action)
                         + self.new_place_reward()
-                        + self.win_loss_reward()
+                        # + self.win_loss_reward()
                         + self.obj_reward()
-                        # + self.change_world_reward()
+                        + self.change_world_reward()
                         )
         return my_step_reward
     
@@ -182,115 +179,3 @@ class ZorkEnvWrapper:
     def get_score(self):
         ''' Returns the current integer game score  '''
         return self.env.get_score()
-    
-
-        
-    
-# 6.可能没必要: 增加一个存活时间奖励
-# 7.可能没必要(得分情况太稀疏了): 连续 no_score_time 步没有得分惩罚,
-# # 倒是可以设置一个 连续 no_score_time 步没有正向 reward -》 惩罚
-
-# 玩家物品？ 不知道这个返回值是什么形式
-    def get_player_object(self):
-        ''' Returns the :class:`jericho.ZObject` corresponding to the player. '''
-        return self.env.get_object(self.env.player_obj_num)
-
-# 输入编号， 返回物品
-    def get_object(self, obj_num):
-        '''
-        Returns a :class:`jericho.ZObject` with the corresponding number or `None` if the object\
-        doesn't exist in the :doc:`object_tree`.
-
-        :param obj_num: Object number between 0 and len(get_world_objects()).
-        :type obj_num: int
-        '''
-        return self.env.get_object(obj_num)
-    
-
-# 查看游戏种子, 没什么用
-    def seed(self, seed=None):
-        '''
-        Changes seed used for the emulator's random number generator.
-
-        :param seed: Seed the random number generator used by the emulator.
-                     Default: use walkthrough's seed if it exists,
-                              otherwise use value of -1 which changes with time.
-        :returns: The value of the seed.
-
-        .. note:: :meth:`jericho.FrotzEnv.reset()` must be called before the seed takes effect.
-        '''
-        return self.env.seed(seed)
-# 这个函数在评什么分？ 似乎有用
-    def _score_object_names(self, interactive_objs):
-        """ Attempts to choose a sensible name for an object, typically a noun. """
-        def score_fn(obj):
-            score = -.01 * len(obj[0])
-            if obj[1] == 'NOUN':
-                score += 1
-            if obj[1] == 'PROPN':
-                score += .5
-            if obj[1] == 'ADJ':
-                score += 0
-            if obj[2] == 'OBJTREE':
-                score += .1
-            return score
-        best_names = []
-        for desc, objs in interactive_objs.items():
-            sorted_objs = sorted(objs, key=score_fn, reverse=True)
-            best_names.append(sorted_objs[0][0])
-        return best_names
-
-
-# 辨认可交互的 物品/地点
-    def _identify_interactive_objects(self, observation='', use_object_tree=False):
-        """
-        Identifies objects in the current location and inventory that are likely
-        to be interactive.
-        确定当前地点和库存中可能是可交互的对象。
-
-        :param observation: (optional) narrative response to the last action, used to extract candidate objects.
-        :param observation: （可选）对上一个动作的叙述响应，用于提取候选对象。
-        :type observation: string
-        :type observation: 字符串
-        :param use_object_tree: Query the :doc:`object_tree` for names of surrounding objects.
-        :param use_object_tree: 查询 :doc:`object_tree` 以获取周围对象的名称。
-        :type use_object_tree: boolean
-        :type use_object_tree: 布尔值
-        :returns: A list-of-lists containing the name(s) for each interactive object.
-        :returns: 一个包含每个可交互对象名称的列表的列表。
-
-        :Example:
-        :示例:
-
-        >>> from jericho import *
-        >>> env = FrotzEnv('zork1.z5')
-        >>> obs, info = env.reset()
-        'You are standing in an open field west of a white house with a boarded front door. There is a small mailbox here.'
-        >>> env.identify_interactive_objects(obs)
-        [['mailbox', 'small'], ['boarded', 'front', 'door'], ['white', 'house']]
-
-        .. note:: Many objects may be referred to in a variety of ways, such as\
-        Zork1's brass latern which may be referred to either as *brass* or *lantern*.\
-        This method groups all such aliases together into a list for each object.
-        
-        .. 注意:: 许多对象可能有多种称呼，例如 Zork1 的黄铜灯笼可以称为 *brass* 或 *lantern*。\
-        此方法将所有这些别名分组到每个对象的一个列表中。
-        """
-        return self.env._identify_interactive_objects(observation, use_object_tree)
-
-# 依旧不知道咋用
-    def _filter_candidate_actions(self, candidate_actions, use_ctypes=False, use_parallel=False):
-        """
-        给定一个候选动作列表，返回一个字典，将世界差异映射到导致该差异的候选动作列表。
-        仅返回导致有效世界差异的动作。
-
-        :param candidate_actions: 要测试 validity 的 candidate action
-        :type candidate_actions: list
-        :param use_ctypes: 使用优化的 ctypes 实现来过滤有效动作。
-        :type use_ctypes: boolean
-        :param use_parallel: 使用并行化实现来过滤有效动作。
-        :type use_parallel: boolean
-        :returns: 世界差异到动作列表的字典。
-        """
-        return self.env._filter_candidate_actions(candidate_actions, use_ctypes, use_parallel)
-        
